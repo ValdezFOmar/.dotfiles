46c2d4891f992f71125aaabecb333b8c8d732dda
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-08-02 00:38:27 -0700

    feat(neovim): Fix lazy installation

--- user/.config/nvim/lua/bones/init.lua
+++ user/.config/nvim/lua/bones/init.lua
@@ -6,14 +6,22 @@ require 'bones.commands'
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not vim.uv.fs_stat(lazypath) then
-    vim.system {
+    local cmd = vim.system({
         'git',
         'clone',
         '--filter=blob:none',
         'https://github.com/folke/lazy.nvim.git',
         '--branch=stable', -- latest stable release
         lazypath,
-    }
+    }, { text = true }):wait(5 * 60 * 1000) -- 5 minutes
+
+    if cmd.code ~= 0 then
+        vim.notify(
+            ('Error installing lazy.nvim: [exit %d] %s'):format(cmd.code, cmd.stderr or ''),
+            vim.log.levels.ERROR
+        )
+        return
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
