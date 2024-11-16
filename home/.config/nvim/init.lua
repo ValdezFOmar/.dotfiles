87a86252487b94e65dc45a0daaec8fe529465254
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-11-16 02:50:17 -0800

    refactor(neovim): move blankline mappings to init.lua

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -203,6 +203,23 @@ autocmd('FileType', {
     end,
 })
 
+autocmd({ 'BufNewFile', 'BufRead' }, {
+    group = augroup('InsertBlankLineMappings', {}),
+    callback = function(event)
+        if vim.fn.getcmdwintype() ~= '' then
+            return -- Don't override <Enter> mappings when editing commands
+        end
+        ---@type integer
+        local bufnr = event.buf
+        local bo = vim.bo[bufnr]
+        if not bo.modifiable or bo.readonly then
+            return
+        end
+        map('n', '<S-Enter>', 'mzO<Esc>0"_D`z', { buffer = bufnr })
+        map('n', '<Enter>', 'mzo<Esc>0"_D`z', { buffer = bufnr })
+    end,
+})
+
 autocmd('TextYankPost', {
     group = augroup('HighlightYank', {}),
     pattern = '*',
