d161c72ebbd564827b05e15642a77e409f231d8d
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-12-15 14:49:16 -0800

    feat(neovim): update queries
    - Make Python SQL injection query more general
    - Include the full text (including escape sequences) in shell injections
      for package.json

--- home/.config/nvim/queries/json/injections.scm
+++ home/.config/nvim/queries/json/injections.scm
@@ -8,7 +8,8 @@
         (string_content) @_key)
       value: (object
         (pair
-          value: (string
-            (string_content) @injection.content))))
+          value: (string) @injection.content)))
     (#eq? @_key "scripts")
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.include-children)
     (#set! injection.language "bash")))
--- home/.config/nvim/queries/python/injections.scm
+++ home/.config/nvim/queries/python/injections.scm
@@ -3,17 +3,10 @@
 ; sqlite3 module `execute` calls
 ((call
   function: (attribute
-    object: [
-      (identifier) @_cur
-      ; In an instance field, like `self.connection.execute`
-      (attribute
-        attribute: (identifier) @_cur)
-    ]
     attribute: (identifier) @_exec)
   arguments: (argument_list
     .
     (string
       (string_content) @injection.content)))
-  (#any-of? @_cur "cur" "cursor" "con" "connection")
   (#any-of? @_exec "execute" "executemany")
   (#set! injection.language "sql"))
