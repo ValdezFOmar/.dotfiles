0eaf453dda6a1c97cf578b7bd44752e05d0c2033
Omar Valdez <omarantoniovaldezf2@gmail.com>
2024-12-16 15:52:46 -0800

    feat(git): remove unnecessary commands and don't use bat for diffs

--- home/bin/common-git-utils.sh
+++ home/bin/common-git-utils.sh
@@ -8,78 +8,50 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-die() {
-    # Bash parameter substitution
-    local prog="${BASH_SOURCE[0]##*/}"
-    >&2 echo "${prog}:" "$@"
+readonly program=${0##*/}
+
+function die() {
+    >&2 echo "$program: error: $*"
     exit 1
 }
 
-recover-file() {
-    local commit file=$1
-
-    [[ -e $file ]] && die "File '$file' exists"
-
-    commit=$(git rev-list -n 1 HEAD -- "$file")
-    [[ ! $commit ]] && die "No commit found for '$file'"
-
-    # get the file from the commit prior to its deletion
-    git checkout "${commit}~1" -- "$file"
-}
-
-bat-diff() {
-    declare -a files
-    mapfile -t files < <(git diff --name-only)
-
-    if [[ ${#files[@]} -ne 0 ]]; then
-        bat --diff "${files[@]}"
-    else
-        echo 'No files to show'
-    fi
-}
-
-intersection-diff() {
+function intersection-diff() {
     # Show the diff for files that are staged and also have unstaged
     # modifications. Useful for reviewing changes made by pre-commit
     declare -a files
-
-    mapfile -t files < <(comm -12 \
-        <(git diff --name-only | sort) \
-        <(git diff --name-only --cached | sort))
+    mapfile -t files < <(comm -12 <(git diff --name-only | sort) <(git diff --name-only --cached | sort))
 
     if [[ ${#files[@]} -ne 0 ]]; then
-        bat --diff "${files[@]}"
+        git diff "${files[@]}"
     else
-        echo 'No files to show'
+        echo "$program: no files to show"
     fi
 }
 
-restage-files() {
+function restage-files() {
     # Similar to `intersection-diff`, but stages the files instead
     declare -a files
-
-    mapfile -t files < <(comm -12 \
-        <(git diff --name-only | sort) \
-        <(git diff --name-only --cached | sort))
+    mapfile -t files < <(comm -12 <(git diff --name-only | sort) <(git diff --name-only --cached | sort))
 
     if [[ ${#files[@]} -ne 0 ]]; then
         git add "${files[@]}"
     else
-        echo 'No files to restage'
+        echo "$program: no files to restage"
     fi
 }
 
-main() {
-    case "$1" in
-        --recover-file)
-            [[ ! $2 ]] && die 'No file provided'
-            recover-file "$2"
-            ;;
-        --bat-diff) bat-diff ;;
-        --intersection-diff) intersection-diff ;;
-        --restage-files) restage-files ;;
-        *) die "Unsupported option '$1'" ;;
-    esac
+function main() {
+    if [[ $# -eq 0 ]]; then
+        die 'no option provided'
+    elif [[ $# -eq 1 ]]; then
+        case "$1" in
+            --intersection-diff) intersection-diff ;;
+            --restage-files) restage-files ;;
+            *) die "unsupported option: $1" ;;
+        esac
+    else
+        die 'too many options provided'
+    fi
 }
 
 main "$@"
