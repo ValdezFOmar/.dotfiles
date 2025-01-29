0028ded8fb8760b1cc983fa53afefd3ba5cf1665
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-01-28 20:16:55 -0800

    feat(hyprland): add toggle for dimming inactive windows

---
+++ home/.config/hypr/scripts/toggle-dim.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# the awk command prints the second column of the first line
+is_dim_active=$(hyprctl getoption decoration:dim_inactive | awk 'NR==1{print $2}')
+
+if [[ $is_dim_active == 1 ]]; then
+    hyprctl keyword decoration:dim_inactive false
+else
+    hyprctl keyword decoration:dim_inactive true
+fi
