69f4a7bde7080cadf2c01b25371822909ea25788
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-02-16 21:36:43 -0800

    refactor(neovim): remove mason and setup servers manually

--- home/.config/nvim/lua/bones/plugins/lsp.lua
+++ home/.config/nvim/lua/bones/plugins/lsp.lua
@@ -1,90 +1,50 @@
 ---@type LazyPluginSpec
-local PLUGIN = { 'williamboman/mason.nvim' }
+local PLUGIN = { 'neovim/nvim-lspconfig' }
 
 PLUGIN.lazy = false
-PLUGIN.dependencies = {
-    'williamboman/mason-lspconfig.nvim',
-    'neovim/nvim-lspconfig',
-}
-
-local servers = {
-    'basedpyright',
-    'bashls', -- needs 'shellcheck' and 'shfmt'
-    'cssls',
-    'emmet_ls',
-    'html',
-    'jsonls',
-    'marksman',
-    'ts_ls',
-}
-
--- Skip this servers in TERMUX since the platform is unsupported
-if vim.uv.os_uname().machine ~= 'aarch64' then
-    table.insert(servers, 'lua_ls')
-    table.insert(servers, 'rust_analyzer')
-end
-
----Setup a server with cmp default capabilities
----@param config_name string
----@param config table?
-local function setup(config_name, config)
-    config = config or {}
-    config.capabilities = require('cmp_nvim_lsp').default_capabilities()
-    require('lspconfig')[config_name].setup(config)
-end
-
----@param config table?
----@return fun(config_name: string)
-local function handler(config)
-    return function(config_name)
-        setup(config_name, config)
-    end
-end
 
 function PLUGIN.config()
-    require('mason').setup {
-        ui = {
-            border = 'rounded',
-            icons = {
-                package_installed = '✔',
-                package_pending = '',
-                package_uninstalled = '✗',
-            },
-        },
-    }
+    local lspconfig = require 'lspconfig'
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local data_path = vim.fn.stdpath 'data' --[[@as string]]
 
-    require('mason-lspconfig').setup {
-        ensure_installed = servers,
-        handlers = {
-            -- default handler
-            handler(),
-            html = handler { filetypes = { 'html', 'templ', 'htmldjango' } },
-            basedpyright = handler {
-                settings = {
-                    basedpyright = {
-                        analysis = {
-                            autoSearchPaths = true,
-                            useLibraryCodeForTypes = true,
-                            diagnosticMode = 'openFilesOnly',
-                            typeCheckingMode = 'standard',
-                        },
+    ---@type table<string, table>
+    local servers = {
+        bashls = {}, -- needs 'shellcheck' and 'shfmt'
+        cssls = {},
+        emmet_language_server = {},
+        eslint = {},
+        html = {},
+        jsonls = {},
+        lua_ls = {},
+        marksman = {},
+        rust_analyzer = {},
+        ts_ls = {},
+        basedpyright = {
+            settings = {
+                basedpyright = {
+                    analysis = {
+                        autoSearchPaths = true,
+                        useLibraryCodeForTypes = true,
+                        diagnosticMode = 'openFilesOnly',
+                        typeCheckingMode = 'standard',
                     },
                 },
             },
         },
-    }
-
-    if vim.fn.executable 'ts_query_ls' == 1 then
-        local data_path = vim.fn.stdpath 'data' --[[@as string]]
-
-        setup('ts_query_ls', {
+        ts_query_ls = {
             settings = {
                 parser_install_directories = {
                     vim.fs.joinpath(vim.fn.getcwd(), 'parser'),
                     vim.fs.joinpath(data_path, 'lazy', 'nvim-treesitter', 'parser'),
                 },
             },
-        })
+        },
+    }
+
+    for server, config in pairs(servers) do
+        config.capabilities = capabilities
+        lspconfig[server].setup(config)
     end
 end
 
