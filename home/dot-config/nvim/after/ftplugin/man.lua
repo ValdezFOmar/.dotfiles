40ad7fd32d706a640458bbc7a34a83200297e281
Omar Valdez <omarantoniovaldezf2@gmail.com>
2025-04-25 04:21:58 -0700

    reafactor(neovim): simplify man ftplugin

--- home/dot-config/nvim/after/ftplugin/man.lua
+++ home/dot-config/nvim/after/ftplugin/man.lua
@@ -1,4 +1,5 @@
-local winid = vim.api.nvim_get_current_win()
+local api = vim.api
+local winid = api.nvim_get_current_win()
 local wo = vim.wo[winid][0]
 
 local function toggle_cursorline()
@@ -14,10 +15,8 @@ vim.keymap.set('n', 's', toggle_cursorline, { buffer = true })
 
 -- Position cursor in the middle (vertically) of the buffer
 do
-    local buffer = vim.api.nvim_win_get_buf(winid)
-    local window_height = vim.api.nvim_win_get_height(winid)
-    local buffer_height = vim.api.nvim_buf_line_count(buffer)
-    local height = (buffer_height < window_height) and buffer_height or window_height
+    local bufnr = api.nvim_win_get_buf(winid)
+    local height = math.min(api.nvim_buf_line_count(bufnr), api.nvim_win_get_height(winid))
     local line = math.ceil(height / 2)
-    vim.api.nvim_win_set_cursor(winid, { line, 0 })
+    api.nvim_win_set_cursor(winid, { line, 0 })
 end
